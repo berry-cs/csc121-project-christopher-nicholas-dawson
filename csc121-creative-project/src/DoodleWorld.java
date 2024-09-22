3a933c393f1848b42d00a0ac10ea9371673d8976
nico4040 <nicholas.koch@vikings.berry.edu>
2024-09-22 12:30:45 -0400

    Added equals to

--- csc121-creative-project/src/DoodleWorld.java
+++ csc121-creative-project/src/DoodleWorld.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 /** represent the state of our doodle jump animation */
 public class DoodleWorld implements IWorld {
@@ -9,6 +10,23 @@ public class DoodleWorld implements IWorld {
 			super();
 			this.x = x;
 			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(x, y);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			DoodleWorld other = (DoodleWorld) obj;
+			return x == other.x && y == other.y;
 		}
 
 }
