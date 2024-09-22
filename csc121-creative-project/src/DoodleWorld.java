7048d788c32252ffd475bac250f9d13a2b6fe1fc
nico4040 <nicholas.koch@vikings.berry.edu>
2024-09-22 12:46:23 -0400

    Created all of the classes

--- csc121-creative-project/src/DoodleWorld.java
+++ csc121-creative-project/src/DoodleWorld.java
@@ -3,31 +3,42 @@ import java.util.Objects;
 /** represent the state of our doodle jump animation */
 public class DoodleWorld implements IWorld {
 	
-		int x;
-		int y;
+	Jumper jumper;
+    IPlatform platforms;
+    Star star;
+    IObstical obsticals;
+    Score score;
+    
+	public DoodleWorld(Jumper jumper, IPlatform platforms, Star star, IObstical obsticals, Score score) {
+		super();
+		this.jumper = jumper;
+		this.platforms = platforms;
+		this.star = star;
+		this.obsticals = obsticals;
+		this.score = score;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(jumper, obsticals, platforms, score, star);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DoodleWorld other = (DoodleWorld) obj;
+		return Objects.equals(jumper, other.jumper) && Objects.equals(obsticals, other.obsticals)
+				&& Objects.equals(platforms, other.platforms) && Objects.equals(score, other.score)
+				&& Objects.equals(star, other.star);
+	}
+    
 		
-		public DoodleWorld(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(x, y);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			DoodleWorld other = (DoodleWorld) obj;
-			return x == other.x && y == other.y;
-		}
+		
 
 }
 
