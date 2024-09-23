205bb01c39710d73b44fb37669e691379824d042
nico4040 <nicholas.koch@vikings.berry.edu>
2024-09-23 10:08:02 -0400

    Removed the interface for platform and obstacle

---
+++ csc121-creative-project/src/Obstacle.java
@@ -0,0 +1,17 @@
+/** Represents a still obstacle*/
+public class Obstacle {
+
+	Posn posn;
+	boolean collision;
+	String color;
+	
+	public Obstacle(Posn posn, boolean collision, String color) {
+		super();
+		this.posn = posn;
+		this.collision = collision;
+		this.color = color;
+	}
+	
+	
+
+}
---
+++ csc121-creative-project/src/Platform.java
@@ -0,0 +1,18 @@
+/** Represents a still platform*/
+public class Platform {
+
+	Posn posn;
+	int width;
+	int height;
+	String color;
+	
+	public Platform(Posn posn, int width, int height, String color) {
+		super();
+		this.posn = posn;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	
+	
+}
