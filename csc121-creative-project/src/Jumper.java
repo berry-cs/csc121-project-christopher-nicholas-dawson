f6341787bed77b86c153186fa52f5b06f573ce2f
Anon517-png <cdowens4505@gmail.com>
2024-09-23 09:59:07 -0400

    Update Jumper.java

--- csc121-creative-project/src/Jumper.java
+++ csc121-creative-project/src/Jumper.java
@@ -2,18 +2,15 @@ import java.util.Objects;
 
 public class Jumper {
 	Posn Jumper;
-	
-	int Jump_factor = 10;
-	
-	Jumper(Posn jumper, int jump_factor) {
+
+	Jumper(Posn jumper) {
 		super();
 		Jumper = jumper;
-		Jump_factor = jump_factor;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(Jump_factor, Jumper);
+		return Objects.hash(Jumper);
 	}
 
 	@Override
@@ -25,11 +22,9 @@ public class Jumper {
 		if (getClass() != obj.getClass())
 			return false;
 		Jumper other = (Jumper) obj;
-		return Jump_factor == other.Jump_factor && Objects.equals(Jumper, other.Jumper);
+		return Objects.equals(Jumper, other.Jumper);
 	}
+	
 
-	
-	
-	
 	
 }
