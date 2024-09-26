9420634110109806b682d908e427b29d34a64449
Anon517-png <cdowens4505@gmail.com>
2024-09-26 12:55:06 -0400

    Update DoodleWorldTest.java

--- csc121-creative-project/src/DoodleWorldTest.java
+++ csc121-creative-project/src/DoodleWorldTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import processing.event.KeyEvent;
 
 class DoodleWorldTest {
-Jumper j1 = new Jumper(new Posn(50,50), new Posn(0,0));
-Jumper j2 = new Jumper(new Posn(50,70), new Posn(0,10));
-Jumper j3 = new Jumper(new Posn(100,100), new Posn(5,-10));
+Jumper j1 = new Jumper(new Posn(50,50), new Posn(0,0), 40, 40);
+Jumper j2 = new Jumper(new Posn(50,70), new Posn(0,10), 40, 40);
+Jumper j3 = new Jumper(new Posn(100,100), new Posn(5,-10), 40, 40);
 Platform p1 = new Platform(new Posn(50,50), 10, 4, "blue");
 DoodleWorld w1 = new DoodleWorld(j1, p1);
 
