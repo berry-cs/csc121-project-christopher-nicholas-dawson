3a7b6ed44a86673903bac388d650e04a268527d1
Anon517-png <cdowens4505@gmail.com>
2024-09-26 12:55:13 -0400

    Update JumperTest.java

--- csc121-creative-project/src/JumperTest.java
+++ csc121-creative-project/src/JumperTest.java
@@ -6,17 +6,17 @@ class JumperTest {
 	
 	// positive y velocity means moving down
 	
-	Jumper j1 = new Jumper(new Posn(50,50), new Posn(0,0));
-	Jumper j2 = new Jumper(new Posn(50,70), new Posn(0,10));
-	Jumper j3 = new Jumper(new Posn(100,100), new Posn(5,-10));
-
+	Jumper j1 = new Jumper(new Posn(50,50), new Posn(0,0), 40, 40);
+	Jumper j2 = new Jumper(new Posn(50,70), new Posn(0,10), 40, 40);
+	Jumper j3 = new Jumper(new Posn(100,100), new Posn(5,-10), 40, 40);
+	Platform p1 = new Platform(new Posn(50,50), 10, 4, "blue");
 	
 	
 	@Test
 	void moveTest() {
-		assertEquals( new Jumper(new Posn(50,50), new Posn(0,1)) ,  j1.move()  );
-		assertEquals(  new Jumper(new Posn(50,80), new Posn(0,11))   ,  j2.move()  );
-		assertEquals(  new Jumper(new Posn(105,90), new Posn(5,-9))   ,  j3.move()  );
+		assertEquals( new Jumper(new Posn(50,50), new Posn(0,1), 40, 40) ,  j1.move(p1)  );
+		assertEquals(  new Jumper(new Posn(50,80), new Posn(0,11), 40, 40)   ,  j2.move(p1)  );
+		assertEquals(  new Jumper(new Posn(105,90), new Posn(5,-9), 40, 40)   ,  j3.move(p1)  );
 		// move()  adds the velocity to the location to change the loction;  y velocity is affected by gravity
 	}
 	
@@ -24,8 +24,8 @@ class JumperTest {
 	
 	@Test
 	void boostTest() {
-		assertEquals( new Jumper(new Posn(50,50), new Posn(0,-4))  , j1.boost() );     
-		assertEquals( new Jumper(new Posn(50, 80), new Posn(0, 6))  , j2.boost() );
+		assertEquals( new Jumper(new Posn(50,50), new Posn(0,-4), 40, 40)  , j1.boost() );     
+		assertEquals( new Jumper(new Posn(50, 80), new Posn(0, 6), 40, 40)  , j2.boost() );
 		// i.e. boost() changes the velocity only  (adds 10 to the y part)
 	}
 	
