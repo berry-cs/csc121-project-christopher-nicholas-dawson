3e17bc6637b1cfa2dbab951d998256aedc76b430
Anon517-png <cdowens4505@gmail.com>
2024-11-25 09:22:13 -0500

    Update Star.java

--- csc121-creative-project/src/Star.java
+++ csc121-creative-project/src/Star.java
@@ -1,18 +1,53 @@
 import java.util.Objects;
+import java.util.Random;
+
+import processing.core.PApplet;
 
 public class Star {
-	private Posn star;
-	private int star_num;
+	private Posn posn;
+	private int width;
+	private int height;
+	private String color;
 
-	Star(Posn star, int star_num) {
+	Star(Posn posn, int width, int height, String color) {
 		super();
-		this.star = star;
-		this.star_num = star_num;
+		this.posn = posn;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	
+	public Star (int ypos) {
+		this( new Posn(new Random().nextInt(400 - 200), ypos), 35, 35, "red" );
+	}
+	// draws the obstacle
+	public PApplet draw(PApplet c) {
+//	    c.imageMode(PApplet.CENTER);
+//	    PImage monsterImg = c.loadImage("monster.png");
+//	    float scaledWidth = monsterImg.width * 0.2f; 
+//	    float scaledHeight = monsterImg.height * 0.2f; 
+//	    c.image(monsterImg, (float)this.posn.getX(), (float)this.posn.getY(), scaledWidth, scaledHeight);
+		c.fill(255, 0, 0);
+		c.rect((int)this.getPosn().getX(), (int)this.getPosn().getY(), this.getWidth(), this.getHeight(), 28);
+		return c;
+	}
+	
+	// returns a posn
+	public Posn getPosn() {
+		return posn;
+	}
+	// returns a width
+	public int getWidth() {
+		return width;
+	}
+	// returns a height
+	public int getHeight() {
+		return height;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(star, star_num);
+		return Objects.hash(color, height, posn, width);
 	}
 
 	@Override
@@ -24,8 +59,10 @@ public class Star {
 		if (getClass() != obj.getClass())
 			return false;
 		Star other = (Star) obj;
-		return Objects.equals(star, other.star) && star_num == other.star_num;
+		return Objects.equals(color, other.color) && height == other.height && Objects.equals(posn, other.posn)
+				&& width == other.width;
 	}
+
 
 
 
