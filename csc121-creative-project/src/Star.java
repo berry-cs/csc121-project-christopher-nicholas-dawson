13b88e4b997b791c939f6f6c254391a44847123c
Anon517-png <cdowens4505@gmail.com>
2024-11-25 09:37:39 -0500

    Update Star.java

--- csc121-creative-project/src/Star.java
+++ csc121-creative-project/src/Star.java
@@ -2,6 +2,10 @@ import java.util.Objects;
 import java.util.Random;
 
 import processing.core.PApplet;
+<<<<<<< HEAD
+=======
+import processing.core.PImage;
+>>>>>>> a387b8e2d50898c691975484f802018199699fb3
 
 public class Star {
 	private Posn posn;
@@ -9,7 +13,11 @@ public class Star {
 	private int height;
 	private String color;
 	
+<<<<<<< HEAD
 	private static final int SIZE = 400;
+=======
+	static PImage starImg = null;
+>>>>>>> a387b8e2d50898c691975484f802018199699fb3
 
 	Star(Posn posn, int width, int height, String color) {
 		super();
@@ -17,6 +25,7 @@ public class Star {
 		this.width = width;
 		this.height = height;
 		this.color = color;
+<<<<<<< HEAD
 	}
 	
 	public Star (int ypos) {
@@ -67,5 +76,64 @@ public class Star {
 
 
 
+
+=======
+	}
+
+public Star(int ypos) {
+	this(new Posn(new Random().nextInt(400 - 200), ypos), 35, 35, "yellow");
+>>>>>>> a387b8e2d50898c691975484f802018199699fb3
+}
+
+public PApplet draw(PApplet c) {
+	if (starImg == null) {
+		starImg = c.loadImage("star.png");
+	}
+	
+	c.imageMode(PApplet.CENTER);
+	
+	float scaleWidth = starImg.width * 0.2f;
+	float scaleHeight = starImg.height * 0.2f;
+	c.image(starImg, (float)this.posn.getX(), (float)this.posn.getY(), scaleWidth, scaleHeight);
+	return c;
+}
+
+
+
+// returns a posn
+public Posn getPosn() {
+	return posn;
+}
+// returns a width
+public int getWidth() {
+	return width;
+}
+// returns a height
+public int getHeight() {
+	return height;
+}
+
+@Override
+public int hashCode() {
+	return Objects.hash(color, height, posn, width);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Star other = (Star) obj;
+	return Objects.equals(color, other.color) && height == other.height && Objects.equals(posn, other.posn)
+			&& width == other.width;
+}
+
+@Override
+public String toString() {
+	return "Star [posn=" + posn + ", width=" + width + ", height=" + height + ", color=" + color + "]";
+}
 
 }
