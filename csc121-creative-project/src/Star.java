67ba38cc6fdf75e98fc03a82b3e82d0a6300eda7
Anon517-png <cdowens4505@gmail.com>
2024-11-26 10:14:14 -0500

    Update Star.java

--- csc121-creative-project/src/Star.java
+++ csc121-creative-project/src/Star.java
@@ -11,20 +11,24 @@ public class Star {
 	private Posn posn;
 	private int width;
 	private int height;
+<<<<<<< HEAD
 	private String color;
 
 
 
+=======
+>>>>>>> aad9ddda4d2e1cb62e3d3eb51aca034323a3df62
 	
 
 
 	static PImage starImg = null;
 	private static final int SIZE = 400;
 
-	Star(Posn posn, int width, int height, String color) {
+	Star(Posn posn, int width, int height) {
 		super();
 		this.posn = posn;
 		this.width = width;
+<<<<<<< HEAD
 		this.height = height;
 		this.color = color;
 
@@ -75,6 +79,34 @@ public class Star {
 		return Objects.equals(color, other.color) && height == other.height && Objects.equals(posn, other.posn)
 				&& width == other.width;
 	}
+=======
+		this.height = height; 
+	}
+
+public Star(int ypos) {
+	this(new Posn(new Random().nextInt(400 - 200), ypos), 35, 35);
+}
+
+public PApplet draw(PApplet c) {
+	if (starImg == null) {
+		starImg = c.loadImage("star.png");
+	}
+	
+//	c.fill(255, 0, 0);
+//	c.rect((int)this.getPosn().getX(), (int)this.getPosn().getY(), this.getWidth(), this.getHeight(), 28);
+	c.imageMode(PApplet.CENTER);
+	
+	float scaleWidth = starImg.width * 0.15f;
+	float scaleHeight = starImg.height * 0.15f;
+	c.image(starImg, (float)this.posn.getX(), (float)this.posn.getY(), scaleWidth, scaleHeight);
+	return c;
+}
+>>>>>>> aad9ddda4d2e1cb62e3d3eb51aca034323a3df62
+
+
+
+
+<<<<<<< HEAD
 
 
 
@@ -84,14 +116,28 @@ public class Star {
 
 
 
+=======
+@Override
+public int hashCode() {
+	return Objects.hash(height, posn, width);
+}
 
-
-
-
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Star other = (Star) obj;
+	return height == other.height && Objects.equals(posn, other.posn) && width == other.width;
+}
+>>>>>>> aad9ddda4d2e1cb62e3d3eb51aca034323a3df62
 
 @Override
 public String toString() {
-	return "Star [posn=" + posn + ", width=" + width + ", height=" + height + ", color=" + color + "]";
+	return "Star [posn=" + posn + ", width=" + width + ", height=" + height + "]";
 }
 
 }
